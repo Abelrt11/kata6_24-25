ba0f3f11f88552bf67b0d560b4080d7713430a40
Abel <abel.romerotavio@gmail.com>
2024-12-27 14:20:35 +0000

    Created the WorkingDateRequestAdapter and WorkingDaysRequestAdapter to be able to adapt the request our API will receive.

---
+++ src/main/java/es/ulpgc/es/view/adapters/WorkingDateRequestAdapter.java
@@ -0,0 +1,31 @@
+package es.ulpgc.es.view.adapters;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class WorkingDateRequestAdapter {
+    public WorkingDateCommand.Input inputFor(HttpServletRequest req) {
+        return new WorkingDateCommand.Input() {
+            @Override
+            public LocalDate start() { return LocalDate.parse(req.getParameter("start")); }
+
+            @Override
+            public int workingDays() { return Integer.parseInt(req.getParameter("days")); }
+        };
+    }
+
+    public WorkingDateCommand.Output outputFor(HttpServletResponse res) {
+        return result -> {
+            try {
+                res.getWriter().write(result.toString());
+
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        };
+    }
+}
